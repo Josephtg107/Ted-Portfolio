8597194e92379b58d80136f6af69287264fcc33d
José Teodoro García Facundo <josephtg107@gmail.com>
2024-06-26 21:34:32 -0600

    [Update] Updated the layout to include the StairsTransition view with the new animations

--- ted-portfolio/app/layout.jsx
+++ ted-portfolio/app/layout.jsx
@@ -4,6 +4,7 @@ import "./globals.css";
 //Components
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const JetbrainsMono = JetBrains_Mono({ 
   subsets: ["latin"],
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={JetbrainsMono.variable}>
         <Header />
+        <StairTransition /> 
         <PageTransition>{children}</PageTransition>
         </body>
     </html>
