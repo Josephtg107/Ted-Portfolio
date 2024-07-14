4f3bb023ded48c38b5165a6665a26dfb4a2815d5
Joseph <josephtg107@gmail.com>
2024-07-14 17:54:42 -0500

    [Feature] Coded and styles Homepage

--- ted-portfolio/app/page.jsx
+++ ted-portfolio/app/page.jsx
@@ -1,5 +1,54 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
-export default function Home() {
-  return <main>homepage</main>
+//Components
+import Social from "@/components/Social";
+import Photo from "@/components/Photo";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between
+        xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">iOS Developer</span>
+            <h1 className="h1">
+              Hello I'm <br /> <span className="text-accent">José Teodoro García</span>
+            </h1>
+            <p className="max-w-p[500px] mb-9 text-white/80">
+            iOS Developer | Software Developer <br />
+            Passionate about building innovative iOS apps and exploring new tech. Fitness lover, gamer, and stream enthusiast.
+            </p>
+            {/* Buttons and Social Links */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2 h-16"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social 
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex
+                  justify-center items-center text-accent text-base hover:bg-accent
+                  hover:text-primary hover:transition-all duration-500"
+                  />
+              </div>
+            </div>
+          </div>
+          {/* text */}
+          <div>
+            <Photo />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
- 
+
+export default Home;
